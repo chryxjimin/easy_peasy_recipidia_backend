661e635841f0f745052cb176914b10ead915981d
Your Username <you@example.com>
2021-03-09 18:07:14 -0500

    testing 2

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,5 +1,5 @@
 class RecipeSerializer
   include FastJsonapi::ObjectSerializer
   attributes :title, :description, :image_url, :cuisine_id, :cuisine
-  # belongs_to :cuisine
+  belongs_to :cuisine
 end
