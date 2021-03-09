ba8a8e3a3c37696aef277e9845bab94aa193009f
Your Username <you@example.com>
2021-03-09 12:39:09 -0500

    recipe controller index and create routes

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,2 +1,24 @@
 class Api::V1::RecipesController < ApplicationController
+
+    def index
+        recipes = Recipe.all
+        render json: recipes
+    end
+
+    def create
+        recipe = Recipe.new(recipe_params)
+        if recipe.save
+            render json: recipe, status: :accepted
+        else 
+            redner json: {message: recipe.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+
+
+    private
+
+        def recipe_params
+            params.require(:recipe).permit(:title, :description, :image_url, :cuisine_id)
+        end
+
 end
