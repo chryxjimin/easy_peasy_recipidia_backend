7ac694d9e5b0a5fc890c327e635d1f60e53bb912
Your Username <you@example.com>
2021-03-14 20:38:55 -0400

    added validations to Recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,4 @@
 class Recipe < ApplicationRecord
     belongs_to :cuisine
+    validates_presence_of :title, :description, :image_url
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,4 +12,6 @@ italian = Cuisine.create(name: "Italian")
 mexican = Cuisine.create(name: "Mexican")
 thai = Cuisine.create(name: "Thai")
 
+Recipe.create(title: "PAD THAI", description: "This restaurant-quality Pad Thai recipe is surprisingly easy to make at home and customize with whatever noodles, proteins, veggies, and spice level you prefer. ", image_url: "https://www.gimmesomeoven.com/wp-content/uploads/2019/01/Pad-Thai-Recipe-1.jpg", cuisine_id: thai.id)
+Recipe.create(title: "SPICY MEXICAN RICE", description: "This vegan rice dish can be eaten on its own or, if you want to add some protein, it’s good with beans, crumbled feta, cooked chicken or prawns stirred through. Ready in 30 minutes, it's a great dinner to have up your sleeve, midweek.", image_url: "https://images.immediate.co.uk/production/volatile/sites/2/2020/06/Steven-Joyce-JOS2020026D00012-FS_preview-7786183.jpg?webp=true&quality=90&resize=620%2C413", cuisine_id: mexican.id)
 Recipe.create(title: "PASTA WITH PEAS", description: "This Italian pasta with peas is a simple, creamy, and delicious one-pot dinner recipe that uses minimal ingredients.", image_url: "https://theclevermeal.com/wp-content/uploads/2020/03/pasta-with-peas-4-copy-1-798x1024.jpg", cuisine_id: italian.id)
