6c631ed59c6cd2f6beed84eea05c7895f2463f8a
Your Username <you@example.com>
2021-03-18 17:42:31 -0400

    added code to access cuisines

--- app/adapters/api.rb
+++ app/adapters/api.rb
@@ -10,6 +10,7 @@ class Api
 
 
     def get_recipes
+        #needs cuisine to be passed as an argument 
         url = "https://api.spoonacular.com/recipes/random?apiKey=#{API_KEY}"
         uri = URI.parse(url)
         response_body = uri.read
@@ -18,10 +19,20 @@ class Api
             title = recipe["title"]
             description = recipe["summary"]
             image_url = recipe["image"]
+            if recipe["cuisines"] = []
+                binding.pry
+                recipe["cuisines"] = Cuisine.find_create_by(name: "General")
+            else
+                recipe["cuisines"] = recipe["cuisines"]
+            end
+            
             # ingredients = recipe["extendedIngredients"]["name"]
             # instructions = recipe["instructions"]
-            # binding.pry
-            Recipe.create(title: recipe["title"], description: recipe["summary"], image_url: recipe["image"], cuisine: )
+            binding.pry
+            Recipe.create(title: recipe["title"], description: recipe["summary"], image_url: recipe["image"])
+            #needs cuisine to be passed as an argument 
+            #need to make sure the first letter is in caps"
+
         end
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,14 @@
 Cuisine.destroy_all
 Recipe.destroy_all
 
-Api::Adapter.new.get_recipes
-
 italian = Cuisine.create(name: "Italian")
 mexican = Cuisine.create(name: "Mexican")
 thai = Cuisine.create(name: "Thai")
+general = Cuisine.create(name: "General")
+
+Api::Adapter.new.get_recipes
+
+
 
 Recipe.create(title: "PAD THAI", description: "This restaurant-quality Pad Thai recipe is surprisingly easy to make at home and customize with whatever noodles, proteins, veggies, and spice level you prefer. ", image_url: "https://www.gimmesomeoven.com/wp-content/uploads/2019/01/Pad-Thai-Recipe-1.jpg", cuisine_id: thai.id)
 Recipe.create(title: "SPICY MEXICAN RICE", description: "This vegan rice dish can be eaten on its own or, if you want to add some protein, it’s good with beans, crumbled feta, cooked chicken or prawns stirred through. Ready in 30 minutes, it's a great dinner to have up your sleeve, midweek.", image_url: "https://images.immediate.co.uk/production/volatile/sites/2/2020/06/Steven-Joyce-JOS2020026D00012-FS_preview-7786183.jpg?webp=true&quality=90&resize=620%2C413", cuisine_id: mexican.id)
